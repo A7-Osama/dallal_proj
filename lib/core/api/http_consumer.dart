0f6be0bb99031cfd84fdeff500031fac941a7116
A7-Osama <ao699168@gmail.com>
2026-02-15 23:28:07 +0300

    started maintaining an api consumer for the project

---
+++ lib/core/api/http_consumer.dart
@@ -0,0 +1,37 @@
+import 'package:dallal_proj/core/api/api_consumer.dart';
+
+class HttpConsumer extends ApiConsumer {
+  @override
+  Future delete(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParams,
+  }) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future get(String path, {Object? data, Map<String, dynamic>? queryParams}) {
+    // TODO: implement get
+    throw UnimplementedError();
+  }
+
+  @override
+  Future patch(String path, {Object? data, Map<String, dynamic>? queryParams}) {
+    // TODO: implement patch
+    throw UnimplementedError();
+  }
+
+  @override
+  Future post(String path, {Object? data, Map<String, dynamic>? queryParams}) {
+    // TODO: implement post
+    throw UnimplementedError();
+  }
+
+  @override
+  Future put(String path, {Object? data, Map<String, dynamic>? queryParams}) {
+    // TODO: implement put
+    throw UnimplementedError();
+  }
+}
