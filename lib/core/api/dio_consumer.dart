f0b083d8db3c0c44bcc7b270f539069fd68241a7
A7-Osama <ao699168@gmail.com>
2026-02-16 00:22:47 +0300

    Dio Consumer, dio exceptions an error model classes are handled

--- lib/core/api/dio_consumer.dart
+++ lib/core/api/dio_consumer.dart
@@ -1,37 +1,99 @@
 import 'package:dallal_proj/core/api/api_consumer.dart';
+import 'package:dallal_proj/core/errors/dio_xcept.dart';
+import 'package:dio/dio.dart';
 
 class DioConsumer extends ApiConsumer {
+  final Dio dio;
+
+  DioConsumer({required this.dio});
+
+  @override
+  Future get(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParams,
+  }) async {
+    try {
+      final response = await dio.get(
+        path,
+        data: data,
+        queryParameters: queryParams,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      handleDioExceptions(e);
+    }
+  }
+
+  @override
+  Future post(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParams,
+  }) async {
+    try {
+      final response = await dio.post(
+        path,
+        data: data,
+        queryParameters: queryParams,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      handleDioExceptions(e);
+    }
+  }
+
+  @override
+  Future patch(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParams,
+  }) async {
+    try {
+      final response = await dio.patch(
+        path,
+        data: data,
+        queryParameters: queryParams,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      handleDioExceptions(e);
+    }
+  }
+
+  @override
+  Future put(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParams,
+  }) async {
+    try {
+      final response = await dio.put(
+        path,
+        data: data,
+        queryParameters: queryParams,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      handleDioExceptions(e);
+    }
+  }
+
   @override
   Future delete(
     String path, {
     Object? data,
     Map<String, dynamic>? queryParams,
-  }) {
-    // TODO: implement delete
-    throw UnimplementedError();
-  }
-
-  @override
-  Future get(String path, {Object? data, Map<String, dynamic>? queryParams}) {
-    // TODO: implement get
-    throw UnimplementedError();
-  }
-
-  @override
-  Future patch(String path, {Object? data, Map<String, dynamic>? queryParams}) {
-    // TODO: implement patch
-    throw UnimplementedError();
-  }
-
-  @override
-  Future post(String path, {Object? data, Map<String, dynamic>? queryParams}) {
-    // TODO: implement post
-    throw UnimplementedError();
-  }
-
-  @override
-  Future put(String path, {Object? data, Map<String, dynamic>? queryParams}) {
-    // TODO: implement put
-    throw UnimplementedError();
+  }) async {
+    try {
+      final response = await dio.delete(
+        path,
+        data: data,
+        queryParameters: queryParams,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      handleDioExceptions(e);
+    }
   }
 }
